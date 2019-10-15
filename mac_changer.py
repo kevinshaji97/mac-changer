5533cdde13e7fe27b602de6620e0493a80aabcdc
Kevin Shaji <ckevin.shaji@gmail.com>
2019-10-15 18:36:30 +0530

    Added accepting user input from arguments

--- mac_changer.py
+++ mac_changer.py
@@ -1,7 +1,27 @@
 import subprocess
+import optparse
 
-interface = input("Enter the interface to change >")
-new_mac = input ("Enter the mac to change >")
 
-print("Changing MAC for "+ interface )
-subprocess.call("ifconfig",shell="true")
+def get_arguments():
+    parser = optparse.OptionParser()
+    parser.add_option("-i", "--interface", dest="interface",
+                      help="interface to change its mac_address ")
+    parser.add_option("-m", "--mac", dest="new_mac", help="New Mac Address")
+    (options, arguments) = parser.parse_args()
+    if not options.interface:
+        parser.error("Please specify an interface use --help for more info")
+    elif not options.new_mac:
+        parser.error("Please specify an interface use --help for more info")
+
+    return options
+
+
+def change_mac(interface, new_mac):
+    print("[+] Changing mac_address for " + interface + "  to  " + new_mac)
+    subprocess.call(["ifconfig", interface, "down"])
+    subprocess.call(["ifconfig", interface, "hw", "ether", new_mac])
+    subprocess.call(["ifconfig", interface, "up"])
+
+
+options = get_arguments()
+change_mac(options.interface, options.new_mac)
